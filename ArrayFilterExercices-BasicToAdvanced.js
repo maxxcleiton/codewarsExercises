95b20c25f7a2b7884c931880f3d0e4504f482654
maxxcleiton <maxcleiton@live.com>
2023-03-07 01:21:43 -0300

    new file and first exercise in array filter

---
+++ ArrayFilterExercices-BasicToAdvanced.js
@@ -0,0 +1,25 @@
+// https://coursework.vschool.io/array-filter-exercises/
+
+// 1) Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
+
+// function fiveAndGreaterOnly(arr) {
+    // your code here
+//   }
+  // test
+//   console.log(fiveAndGreaterOnly([3, 6, 8, 2])); /// [6, 8]
+
+//  results >>>>
+//  two ways of doing it:
+
+// 1st:
+//   function fiveAndGreaterOnly(array) {
+//     return array.filter(array => array > 5);
+//    }
+// console.log(fiveAndGreaterOnly([3, 6, 8, 2]));
+
+// 2nd:
+//   function fiveAndGreaterOnly(array) {
+    //     return array > 5;
+    //    }
+
+    // console.log([3, 6, 8, 2].filter(fiveAndGreaterOnly));
