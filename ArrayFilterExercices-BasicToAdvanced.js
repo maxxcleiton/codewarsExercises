db99e90a187e021dc74db8bc10e95adf4957998d
maxxcleiton <maxcleiton@live.com>
2023-03-12 06:46:44 -0300

    add: 4th question data

--- ArrayFilterExercices-BasicToAdvanced.js
+++ ArrayFilterExercices-BasicToAdvanced.js
@@ -53,3 +53,21 @@
 // console.log(fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"]));
 
 // // ["by", "dog", "wolf", "eaten"]
+
+// 4) Given an array of people objects, return a new array that has filtered out all those who don't belong to the club.
+
+// function peopleWhoBelongToTheIlluminati(arr){
+//     // your code here
+//   }
+//   // test
+//   console.log(peopleWhoBelongToTheIlluminati([
+//       { name: "Angelina Jolie", member: true },
+//       { name: "Eric Jones", member: false },
+//       { name: "Paris Hilton", member: true },
+//       { name: "Kayne West", member: false },
+//       { name: "Bob Ziroll", member: true }
+//   ]));
+//   // =>
+//   //[ { name: 'Angelina Jolie', member: true },
+//   //  { name: 'Paris Hilton', member: true },
+//   //  { name: 'Bob Ziroll', member: true } ]
