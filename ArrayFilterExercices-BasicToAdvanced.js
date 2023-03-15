8a57de63c7a567fee58c8cf98341e457b70f1f06
maxxcleiton <maxcleiton@live.com>
2023-03-15 00:07:29 -0300

    5th question added

--- ArrayFilterExercices-BasicToAdvanced.js
+++ ArrayFilterExercices-BasicToAdvanced.js
@@ -90,3 +90,20 @@
 
 // const names = enterprises.map(x => x.name)
 // console.log(names)
+
+// 5) Make a filtered list of all the people who are old enough to see The Matrix (older than 18)
+
+// function ofAge(arr){
+//     // your code here
+//   }
+//   // test
+//   console.log(ofAge([
+//       { name: "Angelina Jolie", age: 80 },
+//       { name: "Eric Jones", age: 2 },
+//       { name: "Paris Hilton", age: 5 },
+//       { name: "Kayne West", age: 16 },
+//       { name: "Bob Ziroll", age: 100 }
+//   ]));
+//   // =>
+//   //[ { name: 'Angelina Jolie', age: 80 },
+//   //  { name: 'Bob Ziroll', age: 100 } ]
